c0733429bc32ea1fb455e6829776d4439cd73d76
Hasan Erken <hasan.erken@gmail.com>
2023-12-12 14:58:06 +0300

    new setup

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ module fiber
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.42.0
+require (
+	github.com/authorizerdev/authorizer-go v0.0.0-20231027161017-0ca850cb0b1d // indirect
+	github.com/gofiber/fiber/v2 v2.42.0
+)
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log/slog"
 	"os"
 
+	"github.com/authorizerdev/authorizer-go"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -20,7 +22,26 @@ func getPort() string {
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	defaultHeaders := map[string]string{}
+
+	AUTHORIZER_URL := os.Getenv("AUTHORIZER_URL")
+	AUTHORIZER_CLIENT_ID := os.Getenv("AUTHORIZER_CLIENT_ID")
+
+	authorizerClient, err := authorizer.NewAuthorizerClient(AUTHORIZER_CLIENT_ID, AUTHORIZER_URL, "", defaultHeaders)
+	if err != nil {
+		slog.Error(err.Error())
+		panic(err)
+	}
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		email := "hasan.erken@gmail.com"
+		authorizerClient.Login(&authorizer.LoginInput{
+			Email: &email,
+			Password: "Sirena79*",
+		})
+		if err != nil {
+			slog.Error("password validation failed")
+		}
 		return c.JSON(fiber.Map{
 			"message": "Hello, Railway! I am Hasan",
 		})
